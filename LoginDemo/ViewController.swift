90dfe79c6ba8cd26e23116395d0a39d450ec1c7f
LACB <luis.cruzbenitez@gmail.com>
2018-04-16 16:12:21 -0500

    The login works

--- LoginDemo/ViewController.swift
+++ LoginDemo/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userName: UITextField!    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func authenticateUser(_ sender: Any) {
+        if userName.text == "gonet" && passwordField.text == "gonet" {
+            let Home = self.storyboard?.instantiateViewController(withIdentifier: "HomeController") as! HomeController
+            self.navigationController?.pushViewController(Home, animated: true)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
