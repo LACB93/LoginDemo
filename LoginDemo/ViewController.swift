afe57dd73ffabf881e4b748a1cae5e9c47e186dc
LACB <luis.cruzbenitez@gmail.com>
2018-04-24 11:37:56 -0500

    Corrections

--- LoginDemo/ViewController.swift
+++ LoginDemo/ViewController.swift
@@ -14,35 +14,16 @@ import RealmSwift
 class ViewController: UIViewController {
     @IBOutlet weak var userName: UITextField!    
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var mensajeLabel: UILabel!
-    @IBOutlet weak var switchController: UISwitch!
     
     let realm = try! Realm()
     var usersList : Results<Users>!
     override func viewDidLoad() {
-        switchController.addTarget(self, action: #selector(self.rememberSwitch), for: .valueChanged)
-        if UserDefaults.standard.bool(forKey: "USUARIOREGISTRADO") == false {
-            userName.text = UserDefaults.standard.string(forKey: "savedUserName")
-            passwordField.text = UserDefaults.standard.string(forKey: "savedPassword")
-        }
         super.viewDidLoad()
         usersList = realm.objects(Users.self)
         print(usersList)
-        if UserDefaults.standard.bool(forKey: "USUARIOREGISTRADO") == false {
-            switchController.setOn(true, animated: false)
-        }
-        else {
-            switchController.setOn(false, animated: false)
-        }
-        
-//        userName.text = UserDefaults.standard.string(forKey: "savedUserName")
-//        passwordField.text = UserDefaults.standard.string(forKey: "savedPassword")
-//        switchController.setOn(true, animated: false)
         if UserDefaults.standard.bool(forKey: "USUARIOREGISTRADO") == true {
             let Home = self.storyboard?.instantiateViewController(withIdentifier: "HomeController") as! HomeController
             self.navigationController?.pushViewController(Home, animated: false)
-        }else {
-            switchController.setOn(false, animated: false)
         }
         
     }
@@ -51,22 +32,12 @@ class ViewController: UIViewController {
     func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let retry = UIAlertAction(title: "Intentar otra vez", style: .default) { (action) in
-            self.mensajeLabel.text = ""
             self.userName.text = ""
             self.passwordField.text = ""
             self.userName.becomeFirstResponder()
         }
         
-//        let cancelar = UIAlertAction(title: "Cancelar", style: .default, handler: {(action) in
-//
-//            self.mensajeLabel.text = ""
-//            self.userName.text = ""
-//            self.passwordField.text = ""
-//
-//        })
-        
         alert.addAction(retry)
-//        alert.addAction(cancelar)
         present(alert, animated: true, completion: nil)
     }
     
@@ -103,16 +74,6 @@ class ViewController: UIViewController {
         present(alerta, animated: true, completion: nil)
     }
     
-    @IBAction func rememberSwitch(_ switchState: UISwitch) {
-        if switchState.isOn{
-            UserDefaults.standard.set(true, forKey: "rememberme")
-            UserDefaults.standard.set(userName.text, forKey: "savedUserName")
-            UserDefaults.standard.set(passwordField.text, forKey: "savedPassword")
-        }else {
-            UserDefaults.standard.set(false, forKey: "USUARIOREGISTRADO")
-            }
-    }
-    
     @IBAction func authenticateUser(_ sender: Any) {
         
         let usrName = userName.text
@@ -134,7 +95,6 @@ class ViewController: UIViewController {
                     print("--- Inicio de sesión de \(usrName!) ---")
                     
                 } else {
-                    self.mensajeLabel.text = "Credenciales Invalidas"
                     self.showAlert(title: "Credenciales Invalidas", message: "Ingresa nuevamente tu email y contraseña")
                 }
              }
