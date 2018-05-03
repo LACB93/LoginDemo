6baae0d26d7b56b640a89f66bfd9509f0bad2c10
LACB <luis.cruzbenitez@gmail.com>
2018-05-03 12:06:30 -0500

    Finish

--- LoginDemo/HomeController.swift
+++ LoginDemo/HomeController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SwiftKeychainWrapper
 import RealmSwift
 
-enum Opiniones: String {
+enum Emojis: String {
     case enojado = "😡"
     case loco = "🤪"
     case feliz = "😀"
@@ -61,83 +61,107 @@ class HomeController: UIViewController{
     @IBAction func ChivasBtn(_ sender: UIButton) {
         sender.setTitleColor(UIColor.white, for: .selected)
         chivas.isSelected = true
-        azul.isEnabled = false
-        pumas.isEnabled = false
-        america.isEnabled = false
+//        azul.isEnabled = false
+//        pumas.isEnabled = false
+//        america.isEnabled = false
+        azul.isSelected = false
+        pumas.isSelected = false
+        america.isSelected = false
     }
     
     @IBAction func AzulBtn(_ sender: UIButton) {
         sender.setTitleColor(UIColor.white, for: .selected)
         azul.isSelected = true
-        chivas.isEnabled = false
-        pumas.isEnabled = false
-        america.isEnabled = false
+//        chivas.isEnabled = false
+//        pumas.isEnabled = false
+//        america.isEnabled = false
+        chivas.isSelected = false
+        pumas.isSelected = false
+        america.isSelected = false
     }
     
     @IBAction func PumasBtn(_ sender: UIButton) {
         sender.setTitleColor(UIColor.white, for: .selected)
         pumas.isSelected = true
-        chivas.isEnabled = false
-        azul.isEnabled = false
-        america.isEnabled = false
+//        chivas.isEnabled = false
+//        azul.isEnabled = false
+//        america.isEnabled = false
+        chivas.isSelected = false
+        azul.isSelected = false
+        america.isSelected = false
     }
     
     @IBAction func AmericaBtn(_ sender: UIButton) {
         sender.setTitleColor(UIColor.white, for: .selected)
         america.isSelected = true
-        chivas.isEnabled = false
-        azul.isEnabled = false
-        pumas.isEnabled = false
+//        chivas.isEnabled = false
+//        azul.isEnabled = false
+//        pumas.isEnabled = false
+        chivas.isSelected = false
+        azul.isSelected = false
+        pumas.isSelected = false
     }
     
     @IBAction func HappyBtn(_ sender: UIButton) {
         sender.setTitleColor(UIColor.white, for: .selected)
         feliz.isSelected = true
-        enamorado.isEnabled = false
-        enojado.isEnabled = false
-        loco.isEnabled = false
+//        enamorado.isEnabled = false
+//        enojado.isEnabled = false
+//        loco.isEnabled = false
+        enamorado.isSelected = false
+        enojado.isSelected = false
+        loco.isSelected = false
     }
     
     @IBAction func LoveBtn(_ sender: UIButton) {
         sender.setTitleColor(UIColor.white, for: .selected)
         enamorado.isSelected = true
-        feliz.isEnabled = false
-        enojado.isEnabled = false
-        loco.isEnabled = false
+//        feliz.isEnabled = false
+//        enojado.isEnabled = false
+//        loco.isEnabled = false
+        feliz.isSelected = false
+        enojado.isSelected = false
+        loco.isSelected = false
     }
     
     @IBAction func AngryBtn(_ sender: UIButton) {
         sender.setTitleColor(UIColor.white, for: .selected)
         enojado.isSelected = true
-        feliz.isEnabled = false
-        enamorado.isEnabled = false
-        loco.isEnabled = false
+//        feliz.isEnabled = false
+//        enamorado.isEnabled = false
+//        loco.isEnabled = false
+        feliz.isSelected = false
+        enamorado.isSelected = false
+        loco.isSelected = false
     }
     
     @IBAction func CrazyBtn(_ sender: UIButton) {
         sender.setTitleColor(UIColor.white, for: .selected)
         loco.isSelected = true
-        feliz.isEnabled = false
-        enamorado.isEnabled = false
-        enojado.isEnabled = false
+//        feliz.isEnabled = false
+//        enamorado.isEnabled = false
+//        enojado.isEnabled = false
+        feliz.isSelected = false
+        enamorado.isSelected = false
+        enojado.isSelected = false
     }
     
     @IBAction func clean(_ sender: UIButton) {
         opinion.text = ""
-        chivas.isEnabled = true
-        azul.isEnabled = true
-        pumas.isEnabled = true
-        america.isEnabled = true
+//        chivas.isEnabled = true
+//        azul.isEnabled = true
+//        pumas.isEnabled = true
+//        america.isEnabled = true
         age.text = ""
         position.text = ""
         chivas.isSelected = false
         azul.isSelected = false
         pumas.isSelected = false
         america.isSelected = false
-        feliz.isEnabled = true
-        enamorado.isEnabled = true
-        enojado.isEnabled = true
-        loco.isEnabled = true
+//        feliz.isEnabled = true
+//        enamorado.isEnabled = true
+//        enojado.isEnabled = true
+//        loco.isEnabled = true
         feliz.isSelected = false
         enamorado.isSelected = false
         enojado.isSelected = false
@@ -145,16 +169,16 @@ class HomeController: UIViewController{
     }
     
    
-    var opinionn: Opiniones = .sinSeleccionar
+    var selectEmoji: Emojis = .sinSeleccionar
     
     @IBAction func EmojiPressed(_ sender: UIButton) {
        
 
         switch sender {
-        case feliz: opinionn = .feliz
-        case enamorado: opinionn = .enamorado
-        case enojado: opinionn = .enojado
-        case loco: opinionn = .loco
+        case feliz: selectEmoji = .feliz
+        case enamorado: selectEmoji = .enamorado
+        case enojado: selectEmoji = .enojado
+        case loco: selectEmoji = .loco
         default: break
         }
     }
@@ -175,11 +199,20 @@ class HomeController: UIViewController{
     
     @IBAction func save(_ sender: UIButton) {
         
+        if opinion.text == "" || age.text == "" || position.text == "" || selectTeam == .sinSeleccionar || selectEmoji == .sinSeleccionar {
+            let alertController = UIAlertController(title: "Error", message: "Por favor llena o selecciona todas las opciones", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
         guard let answer1 = opinion.text else {return}
         if selectTeam == .sinSeleccionar {return}
         let answer2 = selectTeam.rawValue
-        if opinionn == .sinSeleccionar { return }
-        let answer3 = opinionn.rawValue
+        if selectEmoji == .sinSeleccionar { return }
+        let answer3 = selectEmoji.rawValue
         
         guard let answer4 = age.text else {return}
         guard let answer5 = position.text else {return}
@@ -194,10 +227,11 @@ class HomeController: UIViewController{
         
         try! self.realm.write {
             self.realm.add(newAnswer)
-        }
+    
         print(newAnswer)
         print("--- Datos Guardados ---")
         let Last = self.storyboard?.instantiateViewController(withIdentifier: "LastViewController") as! LastViewController
         self.navigationController?.pushViewController(Last, animated: true)
+        }
     }
 }
