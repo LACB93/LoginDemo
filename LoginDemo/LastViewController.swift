bafdcd33753c5e9752f73f0e5c466bbd76728760
LACB <luis.cruzbenitez@gmail.com>
2018-05-08 17:35:28 -0500

    The app was traslated into english

--- LoginDemo/LastViewController.swift
+++ LoginDemo/LastViewController.swift
@@ -6,9 +6,32 @@
 //  Copyright © 2018 Gonet. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Localize_Swift
 
 
 class LastViewController: HomeController {
+    @IBOutlet weak var surverLabel: UILabel!
+    @IBOutlet weak var singOff: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setTexts()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(setTexts), name: NSNotification.Name(LCLLanguageChangeNotification), object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func setTexts(){
+        surverLabel.text = "Gracias por contestar la encuesta".localized()
+        singOff.setTitle("Cerrar Sesión".localized(using: "ButtonsTitles"), for: UIControlState.normal)
+    }
     
 }
